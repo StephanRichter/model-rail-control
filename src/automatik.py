2fb0f139e6b131ebd9503e53fbd6c21621924ca5
Stephan Richter <github@srsoftware.de>
2013-12-01 05:12:39 +0100

    118 kopf machen re

--- src/automatik.py
+++ src/automatik.py
@@ -1832,7 +1832,9 @@ while True:
             else:
                 err()
         elif BR110.stat(BEREIT,LINKS,2):
-            if BR118.stat(ABGEKUPPELT,RECHTS,2):
+            if BR118.stat(ANKUPPELN,RECHTS,2) and BR130.stat(ABGEKUPPELT,LINKS,1) and ICE.stat(BEREIT,RECHTS,4):
+                reset()
+            elif BR118.stat(ABGEKUPPELT,RECHTS,2):
                 if BR130.stat(ABGEKUPPELT,LINKS,1):
                     if ICE.stat(BEREIT,RECHTS,1):
                         rand=random.choice([1,2,3,4,5])                        
@@ -2007,13 +2009,15 @@ while True:
                         err()
                 else:
                     err()
+            elif BR118.stat(UMFAHREN,RECHTS,1) and BR130.stat(ABGEKUPPELT,LINKS,1) and ICE.stat(BEREIT,RECHTS,4):
+                reset()
             else:
                 err()
         elif BR110.stat(BEREIT, RECHTS,1) and BR118.stat(ABGEKUPPELT,RECHTS,2):
             if BR130.stat(ABGEKUPPELT,LINKS,1):
                 if ICE.stat(BEREIT,LINKS,2):
                     rand=random.choice([1,2,3,4,5])
-                    rand=3
+                    rand=4
                     print "z1959"
                     print "rand =",rand
                     if rand==1:
