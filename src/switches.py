cb80499696faf31eca97d7aa8b121facc9a39fd9
Stephan Richter <github@srsoftware.de>
2013-12-03 19:21:56 +0100

    anzugzeiten der Weichen vergrößert

--- src/switches.py
+++ src/switches.py
@@ -27,93 +27,93 @@ weiche10.init("N")
 def ausfahrt1():
     print "Weichenstraße Ausfahrt 1"
     time.sleep(0.5)
-    weiche4.actuate(0, 1)
+    weiche4.actuate(0, 1, 500)
     time.sleep(1)
-    weiche4.actuate(0, 1)
+    weiche4.actuate(0, 1, 500)
     time.sleep(1)
-    weiche2.actuate(0, 1)      
+    weiche2.actuate(0, 1, 500)      
     time.sleep(0.5)
 
 def ausfahrt2():
     print "Weichenstraße Ausfahrt 2"
     time.sleep(0.5)
-    weiche4.actuate(1, 1)
+    weiche4.actuate(1, 1, 500)
     time.sleep(1)
-    weiche2.actuate(0, 1)
+    weiche2.actuate(0, 1, 500)
     time.sleep(0.5)
 
 def ausfahrt3():
     print "Weichenstraße Ausfahrt 3"
     time.sleep(0.5)
-    weiche2.actuate(0, 1)
+    weiche2.actuate(0, 1, 500)
     time.sleep(1)
-    weiche1.actuate(0, 1)
+    weiche1.actuate(0, 1, 500)
     time.sleep(1)
-    weiche3.actuate(0, 1)
+    weiche3.actuate(0, 1, 500)
     time.sleep(1)
-    weiche34.actuate(0, 1)
+    weiche34.actuate(0, 1, 500)
     time.sleep(0.5)
       
 def ausfahrt4():
     print "Weichenstraße Ausfahrt 4"
     time.sleep(0.5)
-    weiche2.actuate(0, 1)
+    weiche2.actuate(0, 1, 500)
     time.sleep(1)
-    weiche1.actuate(0, 1)
+    weiche1.actuate(0, 1, 500)
     time.sleep(1)
-    weiche3.actuate(1, 1)
+    weiche3.actuate(1, 1, 500)
     time.sleep(1)
-    weiche34.actuate(0, 1)
+    weiche34.actuate(0, 1, 500)
     time.sleep(0.5)
 
 def einfahrt1():
     print "Weichenstraße Einfahrt 1"
     time.sleep(0.5)
-    weiche4.actuate(0, 1)
+    weiche4.actuate(0, 1, 500)
     time.sleep(0.5)
-    weiche1.actuate(1, 1)
+    weiche1.actuate(1, 1, 500)
     time.sleep(0.5)
-    weiche5.actuate(0, 1)            
+    weiche5.actuate(0, 1, 500)            
     time.sleep(0.5)
 
 def einfahrt2():
     print "Weichenstraße Einfahrt 2"
     time.sleep(0.5)
-    weiche4.actuate(0, 1)
+    weiche4.actuate(0, 1, 500)
     time.sleep(1)
-    weiche1.actuate(1, 1)
+    weiche1.actuate(1, 1, 500)
     time.sleep(1)
-    weiche1.actuate(1, 1)
+    weiche1.actuate(1, 1, 500)
     time.sleep(1)
-    weiche4.actuate(1, 1)
+    weiche4.actuate(1, 1, 500)
     time.sleep(1)
-    weiche4.actuate(1, 1)            
+    weiche4.actuate(1, 1, 500)            
     time.sleep(1)
-    weiche5.actuate(0, 1)            
+    weiche5.actuate(0, 1, 500)            
     time.sleep(0.5)
 
 def einfahrt3():
     print "Weichenstraße Einfahrt 3"
     time.sleep(0.5)
-    weiche3.actuate(0, 1)
+    weiche3.actuate(0, 1, 500)
     time.sleep(1)
-    weiche1.actuate(0, 1)
+    weiche1.actuate(0, 1, 500)
     time.sleep(1)
-    weiche4.actuate(0, 1)            
+    weiche4.actuate(0, 1, 500)            
     time.sleep(1)
-    weiche34.actuate(0, 1)
+    weiche34.actuate(0, 1, 500)
     time.sleep(0.5)
 
 def einfahrt4():
     print "Weichenstraße Einfahrt 4"
     time.sleep(0.5)
-    weiche3.actuate(1, 1)
+    weiche3.actuate(1, 1, 500)
     time.sleep(1)
-    weiche1.actuate(0, 1)
+    weiche1.actuate(0, 1, 500)
     time.sleep(1)
-    weiche4.actuate(0, 1)            
+    weiche4.actuate(0, 1, 500)            
     time.sleep(1)
-    weiche34.actuate(0, 1)
+    weiche34.actuate(0, 1, 500)
     time.sleep(0.5)
 
 def entkupplenLinks(count=1):
@@ -122,41 +122,41 @@ def entkupplenLinks(count=1):
         time.sleep(0.2*i)
 
 def entkuppeln2():
-    entkuppler2.actuate(1, 1)
+    entkuppler2.actuate(1, 1, 500)
     time.sleep(0.15)
 
 def entkuppeln3():
-    entkuppler3.actuate(0, 1)
+    entkuppler3.actuate(0, 1, 500)
     time.sleep(0.2)
         
 def bahnhofLinksAbzweig():
     print "Weichenstraße Bahnhof (links) Abzweig"
     time.sleep(0.5)
-    weiche10.actuate(0, 1)
+    weiche10.actuate(0, 1, 500)
     time.sleep(1)
-    weiche10.actuate(0, 1)
+    weiche10.actuate(0, 1, 500)
     time.sleep(0.5)
 
 def bahnhofLinksGerade():
     print "Weichenstraße Bahnhof (links) Gerade"
     time.sleep(1)
-    weiche9.actuate(1, 1)
+    weiche9.actuate(1, 1, 500)
     time.sleep(1)
-    weiche9.actuate(1, 1)
+    weiche9.actuate(1, 1, 500)
     time.sleep(1)
-    weiche9.actuate(1, 1)
+    weiche9.actuate(1, 1, 500)
     time.sleep(1)
-    weiche10.actuate(1, 1)
+    weiche10.actuate(1, 1, 500)
     time.sleep(1)
     
 def umfahrungRechts2():
     time.sleep(0.5)
-    weiche12.actuate(1, 1)
+    weiche12.actuate(1, 1, 500)
     time.sleep(0.5)
     
 def umfahrungRechts3():
     time.sleep(0.5)
-    weiche34.actuate(1, 1)
+    weiche34.actuate(1, 1, 500)
     time.sleep(0.5)
 
 
