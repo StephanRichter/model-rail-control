991afbf7bc17ffd6651f40995fa82017f0099411
Stephan Richter <github@srsoftware.de>
2014-03-25 21:56:27 +0100

    mit client- und serversockets rumgespielt

--- src/server.py
+++ src/server.py
@@ -1,35 +1,98 @@
 #!/usr/bin/python
 # coding=utf8
-import socket,sys;
+import socket,sys,time;
+from thread import *
 
-try:
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-except socket.error, msg:
-    print "Failed to create socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
-    sys.exit();
+#try:
+#    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#except socket.error, msg:
+#    print "Failed to create socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
+#    sys.exit();
+#
+#print "socket created"
+#
+#host = 'localhost'
+#port = 4303
+#try:
+#    remote_ip=socket.gethostbyname(host)
+#except:
+#    print "Hostname "+host+" could not be resolved. Exiting"
+#    sys.exit()
+#    
+#print "Ip adress of "+host+" is "+remote_ip
+#
+#s.connect((remote_ip , port))
+# 
+#print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+#
+#welcome = s.recv(1024)
+#print welcome
+#
+#def send(msg):
+#    print "Sending "+msg
+#    try:
+#        s.sendall(msg+"\n")
+#    except socket.error:
+#        print 'Send failed'
+#        sys.exit()
+#    reply = s.recv(1024)
+#    print reply
+#
+#
+#send("SET PROTOCOL SRCP 0.8.4")
+#send("GO")
+#send("INIT 1 GA 1 N")
+#send("SET 1 POWER ON")
+#time.sleep(1)
+#send("SET 1 GA 1 0 1 100")
+#time.sleep(1)
+#send("SET 1 GA 1 1 1 100")
+#time.sleep(1)
+#s.close()
 
-print "socket created"
+HOST=''
+PORT=8888
 
-host = 'www.google.com'
-port = 80
-try:
-    remote_ip=socket.gethostbyname(host)
-except:
-    print "Hostname "+host+" could not be resolved. Exiting"
-    sys.exit()
-    
-print "Ip adress of "+host+" is "+remote_ip
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((remote_ip , port))
+print 'Socket created'
  
-print 'Socket Connected to ' + host + ' on ip ' + remote_ip
-
-message = "GET / HTTP/1.1\r\n\r\n"
-
 try:
-    s.sendall(message)
-except socket.error:
-    print 'Send failed'
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
+     
+print 'Socket bind complete'
+
+s.listen(10)
+print 'Socket now listening'
+
+def clientthread(conn):
+    #Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+     
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:         
+        #Receiving from client
+        data = conn.recv(1024)
+        reply = 'OK...' + data
+        if not data:
+            break
+     
+        conn.sendall(reply)
+     
+    #came out of loop
+    conn.close()
+    print "connection closed"
+
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
  
-print 'Message send successfully'
+s.close()
