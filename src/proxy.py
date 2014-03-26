f3510887bff049086fed9862820cbe283f847e16
Stephan Richter <srichter@modellbahn.(none)>
2014-03-26 09:38:38 +0100

    server.py nach proxy.py verschoben

--- src/proxy.py
+++ src/proxy.py
@@ -2,21 +2,13 @@
 # coding=utf8
 import socket,sys,time
 from thread import start_new_thread
-from time import gmtime, clock
-
 
 serverhost=''
 serverport=4304
-srcphost = 'localhost'
-srcpport = 4303
 
-try:
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-    print "Failed to create server socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
-    sys.exit(-1);
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print 'Server socket created'
+print 'Socket created'
  
 try:
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -27,84 +19,108 @@ except socket.error , msg:
      
 print 'Socket bind complete'
 
-try:
-    srcpip=socket.gethostbyname(srcphost)
-except:
-    print "Hostname "+srcphost+" could not be resolved. Exiting"
-    sys.exit(-3)
-    
-print "Ip adress of "+srcphost+" is "+srcpip
-
 serversocket.listen(10)
-
 print 'Socket now listening'
 
-count = 0
 
-while True:
-    #wait to accept a connection - blocking call
-    client, addr = serversocket.accept()    
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
 
+#for style in xrange(8):
+#        for fg in xrange(30,38):
+#            s1 = ''
+#            for bg in xrange(40,48):
+#                format = ';'.join([str(style), str(fg), str(bg)])
+#                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+#            print s1
+#        print '\n'
+        
+def prnt(text,info):
+    if info:
+        print "\033[1;40;31m"+text+"\033[0m"
+    else:
+        print text
+        
+def sensorThread(source,sink):
+    time.sleep(1)
+    while True:    
+        nb=raw_input("Press ENTER for sensor event")
+        msg=str(time.time())+" 100 INFO 0 FB 64 1";
+        print msg
+        source.sendall(msg+"\n")
+        time.sleep(0.2)               
+        msg=str(time.time())+" 100 INFO 0 FB 64 0";
+        print msg
+        source.sendall(msg+"\n")
+        time.sleep(1)               
+        
+def connectA(source,sink,connection):
+    while True:
+        data=source.recv(1024)
+        if not data:
+            break
+        prnt(data[:-1]+" >>>",infsession[connection])
+        if data=="SET CONNECTIONMODE SRCP INFO\n":
+            infsession[connection]=True
+            start_new_thread(sensorThread, (source,sink,)) 
+        sink.sendall(data)
+    try:
+        source.close()
+    except:
+        pass
+    print "connection closed"    
+    
+def connectB(source,sink,connection):
+    while True:
+        data=source.recv(1024)
+        if not data:
+            break
+        prnt("<<< "+data[:-1],infsession[connection]) 
+        sink.sendall(data)
+    try:
+        source.close()
+    except:
+        pass
+    print "connection closed"
+
+
+def clientthread(client,connection):    
     try:
         srcpsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     except socket.error, msg:
-        print "Failed to create srcp socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
-        sys.exit(-2);
+        print "Failed to create socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
+        return()
+    
+    print "socket created"
+    
+    srcphost = 'localhost'
+    srcpport = 4303
+    try:
+        srcpip=socket.gethostbyname(srcphost)
+    except:
+        print "Hostname "+srcphost+" could not be resolved. Exiting"
+        sys.exit()
+        
+    print "Ip adress of "+srcphost+" is "+srcpip
+    
     srcpsock.connect((srcpip , srcpport))
      
     print 'Socket Connected to ' + srcphost + ' on ip ' + srcpip
     
     welcome = srcpsock.recv(1024)
-    print welcome
-    client.send(welcome)    
-       
-    #infinite loop so that function do not terminate and thread do not end.
-    while True:         
-        #Receiving from client
-        data = client.recv(1024)
-        if not data:
-            break;
-        print data
-        srcpsock.sendall(data)
-        
-        data = srcpsock.recv(1024)
-        if not data:
-            break;
-        print data
-        client.sendall(data)
-        count+=1
-        if count==10:
-            print clock()
-            data="1395801645.766 100 INFO 1 FB 64 1\n"
-            print "interfering: "+data
-            client.sendall(data)
-            data="1395801645.766 100 INFO 2 FB 64 1\n"
-            print "interfering: "+data
-            client.sendall(data)
-            data="1395801645.766 100 INFO 64 1\n"
-            print "interfering: "+data
-            client.sendall(data)
-            data="100 INFO 64 1\n"
-            print "interfering: "+data
-            client.sendall(data)
-        if count==11:
-            data="1395801645.766 100 INFO 1 FB 64 0\n"
-            print "interfering: "+data
-            client.sendall(data)
-            data="1395801645.766 100 INFO 2 FB 64 0\n"
-            print "interfering: "+data
-            client.sendall(data)
-            data="1395801645.766 100 INFO 64 0\n"
-            print "interfering: "+data
-            client.sendall(data)
-            data="100 INFO 64 0\n"
-            print "interfering: "+data
-            client.sendall(data)
-            count=0
-    #came out of loop
-    srcpsock.close()
-    client.close()
-    print "connection closed"
+    print welcome     
+    client.send(welcome)
+    
+    start_new_thread(connectA, (client,srcpsock,connection,))
+    start_new_thread(connectB, (srcpsock,client,connection,))
+    
+connection=0
+infsession=[]
+                     
+while 1:
+    #wait to accept a connection - blocking call
+    client, addr = serversocket.accept()
+    infsession.append(False)
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    start_new_thread(clientthread, (client,connection,))
+    connection+=1    
  
 serversocket.close()
