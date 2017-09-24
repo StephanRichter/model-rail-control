63d0754bc67b8a8e6a6350f223877602a2f83dbb
Stephan Richter <s.richter@keawe.de>
2017-09-24 16:29:07 +0000

    overwritten proxy

--- src/proxy.py
+++ src/proxy.py
@@ -1,185 +1,132 @@
 #!/usr/bin/python
-# coding=utf8
-import socket,sys
+import socket,sys, time
 from thread import start_new_thread
-from mcp23s17 import *
 
+class SRCPSock(object):
 
-def sendAndRcv(sock,message):
-    #print message
-    sock.sendall(message+"\n")
-    reply=sock.recv(1024)    
-    #print reply[:-1]
+    def recv(self):
+        return self.socket.recv(1024);
+    
+    def send(self,message):
+        self.socket.sendall(message+"\n")
+        reply=self.recv()
 
-def prnt(text,info):
-    if info:
-        print "\033[1;40;31m"+text+"\033[0m"
-    else:
-        print text
+    def close(self):
+        self.socket.close();
+        print "Disconnected from SRCP!"
+    
+    def __init__(self,host,port):
+        self.port=port
+        try:
+            self.host=socket.gethostbyname(host)
+        except:
+            print "Hostname "+host+" could not be resolved. Exiting"
+            sys.exit(-2)            
         
-def sensorThread(source,sink):
-    old=0
-    time.sleep(1) # otherwise active sensors would interfer with the handshake
-    while True:
-        val=0
-        for addr in (MCPs):
-            val<<=8
-            val = val|readSPI(addr, SPI_GPIOA)
-            val<<=8
-            val = val|readSPI(addr, SPI_GPIOB)
-
-        diff=old^val
-
-        for i in range(16*len(MCPs),0,-1):
-            if 1<<i-1 & diff:
-                if 1<<i-1 & val:
-                    msg=str(time.time())+" 100 INFO 0 FB "+str(i)+" 1";
-                else:
-                    msg=str(time.time())+" 100 INFO 0 FB "+str(i)+" 0";
-                
-                #prnt(msg,True)
-                source.sendall(msg+"\n")
-        old=val
+    def connect(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            print "Client socket created."        
+        except socket.error, msg:
+            print "Failed to create socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
+            sys.exit(-1)
+        self.socket.connect((self.host , self.port))
+        print "SRCP connected @ {}:{}".format(self.port,self.host);
         
-        time.sleep(0.01)
+        
+    def setup(self):
+        self.connect();
+        welcome = self.recv()
+        print "Connected to SRCP:"
+        print welcome
+        self.send("SET PROTOCOL SRCP 0.8")
+        self.send("SET CONNECTIONMODE SRCP COMMAND")
+        self.send("GO")
+        self.send("SET 1 POWER ON")
+        for addr in range(1,40):
+            time.sleep(0.01)
+            self.send("INIT 1 GL "+str(addr)+" N 1 128 4")
+            self.send("INIT 1 GA "+str(addr)+" N")
+        self.send("TERM 0 SESSION")
+        self.close()
+
+class RocrailProxy(object):
+    def __init__(self,host,port):
+        print "Creating new SRCP proxy @ {}:{}".format(host,port)
+        self.host=host
+        self.port=port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def recv(self):
+        return self.socket.recv(1024)
+    
+    def send(self,data):
+        self.socket.sendall(data)
+        
+    def client2srcp(self,socket,connection):
+        while True:
+            data=socket.recv(1024)
+            if not data:
+                break
+            print "client == {} ==> srcp".format(data.strip());
+            self.srcp.send(data)
+            if data=="SET CONNECTIONMODE SRCP INFO\n":
+                self.infosession[connection]=True
+                start_new_thread(sensorThread)
+        try:
+            print "Closing client socket!"
+            socket.close()
             
-def connectA(source,sink,connection):
-    while True:
-        data=source.recv(1024)
-        if not data:
-            break
-        #prnt(data[:-1]+" >>>",infsession[connection])
-        sink.sendall(data)
-        if data=="SET CONNECTIONMODE SRCP INFO\n":
-            infsession[connection]=True
-            start_new_thread(sensorThread, (source,sink,))
-    try:
-        source.close()
-    except:
-        pass
-    #print "connection closed"    
-    
-def connectB(source,sink,connection):
-    while True:
-        data=source.recv(1024)
-        if not data:
-            break
-        #prnt("<<< "+data[:-1],infsession[connection]) 
-        sink.sendall(data)
-    try:
-        source.close()
-    except:
-        pass
-    #print "connection closed"
-
-
-def clientthread(client,connection):    
-    try:
-        srcpsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    except socket.error, msg:
-        print "Failed to create socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
-        return()
-    
-    #print "socket created"
-    
-    srcphost = 'localhost'
-    srcpport = 4303
-    try:
-        srcpip=socket.gethostbyname(srcphost)
-    except:
-        print "Hostname "+srcphost+" could not be resolved. Exiting"
-        sys.exit(-4)
+        except:
+            pass
         
-    #print "Ip adress of "+srcphost+" is "+srcpip
-    
-    srcpsock.connect((srcpip , srcpport))
-     
-    #print 'Socket Connected to ' + srcphost + ' on ip ' + srcpip
-    
-    welcome = srcpsock.recv(1024)
-    #print welcome     
-    client.send(welcome)
-    
-    start_new_thread(connectA, (client,srcpsock,connection,))
-    start_new_thread(connectB, (srcpsock,client,connection,))
-    
-###########################################################################################
-###########################################################################################
+    def srcp2client(self,socket,connection):
+        while True:
+            data=self.srcp.recv()
+            if not data:
+                break
+            print "client <== {} == srcp".format(data.strip());
+            socket.sendall(data)
+        try:
+            self.srcp.close()
+        except:
+            pass
+        
+    def connect(self,client,connection):    
+        self.srcp.connect();
+        start_new_thread(self.srcp2client, (client,connection,))
+        start_new_thread(self.client2srcp, (client,connection,))
+        
+    def listen(self,srcpSock):
+        try:
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket.bind((self.host,self.port))
+        except socket.error , msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit(-3) 
+        
+        print 'Bound to server socket.'
 
-# Initialisierung der Port-Expander:
-
-MCPs=(3,0,1,2)
-
-activateAdressing()
-for addr in MCPs:
-    initMCP23S17(addr,0xFF,0xFF) # all-read
-
-# Initialisierung des Servers
-
-serverhost=''
-serverport=4304
-
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    srcpsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-except socket.error, msg:
-    print "Failed to create socket. Error code: "+str(msg[0])+", Error message: "+msg[1]
-    sys.exit(-1)
-
-print "socket created"
-
-srcphost = 'localhost'
-srcpport = 4303
-try:
-    srcpip=socket.gethostbyname(srcphost)
-except:
-    print "Hostname "+srcphost+" could not be resolved. Exiting"
-    sys.exit(-2)
-    
-print "Ip adress of "+srcphost+" is "+srcpip
-
-srcpsock.connect((srcpip , srcpport))
- 
-print 'Socket Connected to ' + srcphost + ' on ip ' + srcpip
-
-welcome = srcpsock.recv(1024)
-print welcome
-
-sendAndRcv(srcpsock,"SET PROTOCOL SRCP 0.8")
-sendAndRcv(srcpsock,"SET CONNECTIONMODE SRCP COMMAND")
-sendAndRcv(srcpsock, "GO")
-sendAndRcv(srcpsock, "SET 1 POWER ON")
-for addr in range(1,40):
-    time.sleep(0.01)
-    sendAndRcv(srcpsock, "INIT 1 GL "+str(addr)+" N 1 128 4")
-    sendAndRcv(srcpsock, "INIT 1 GA "+str(addr)+" N")
-sendAndRcv(srcpsock, "TERM 0 SESSION")
-srcpsock.close()
-
-print 'Socket created'
- 
-try:
-    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serversocket.bind((serverhost, serverport))
-except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit(-3)     
-
-print 'Socket bind complete'
-
-serversocket.listen(10)
-print 'Socket now listening'
-    
-connection=0
-infsession=[]
+        self.srcp=srcpSock
+        self.socket.listen(10)
+        print 'Listening.'
+            
+        connection=0
+        self.infoSession=[]
                      
-while 1:
-    #wait to accept a connection - blocking call
-    client, addr = serversocket.accept()
-    infsession.append(False)
-    #print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    start_new_thread(clientthread, (client,connection,))
-    connection+=1    
+        while True:
+            client, addr = self.socket.accept()
+            self.infoSession.append(False)
+            print 'Incoming connection from {}:{}...'.format(addr[0],addr[1]);
+            self.connect(client,connection)
+            connection+=1    
  
-serversocket.close()
+        serversocket.close()
+
+     
+if __name__ == "__main__":
+    srcpSock = SRCPSock('localhost',4303);
+    srcpSock.setup()
+    
+    proxy = RocrailProxy('localhost',4304);
+    proxy.listen(srcpSock)
